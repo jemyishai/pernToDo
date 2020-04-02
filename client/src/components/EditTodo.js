e159c206079cda803fda0c421d0ed2a9f58cc8a5
Jemyishai <jesse.moskowitz@gmail.com>
2020-04-02 12:14:09 -0400

    finished basic compenents

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,41 +1,89 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from "react";
 
-const EditTodo = ({todo}) => {
-  return <Fragment>
-              <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
-              Edit
-            </button>
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
 
+  //edit description function
+  const updateDescription = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+        }
+      );
+       window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
-            <div class="modal" id="myModal">
-              <div class="modal-dialog">
-                <div class="modal-content">
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
 
-
-                  <div class="modal-header">
-                    <h4 class="modal-title">Edit Todos</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  </div>
-
-
-                  <div class="modal-body">
-                    <input type="text" className="form-control"/>
-                  </div>
-
-
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-warning" data-dismiss="modal">
-                    Edit
-                    </button>
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">
-                    Close
-                    </button>
-                  </div>
-
-                </div>
-              </div>
+      <div
+        className="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todos</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                &times;
+              </button>
             </div>
-        </Fragment>;
+
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-warning"
+                data-dismiss="modal"
+                onClick={e => updateDescription(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default EditTodo;
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,40 +1,39 @@
 import React, { Fragment, useEffect, useState } from "react";
 
-import EditTodo from './EditTodo';
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
-  const deleteTodo = async (id) =>{
+  const deleteTodo = async id => {
     try {
-      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`,{
-        method:"DELETE"
-      })
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE"
+      });
 
       setTodos(todos.filter(todo => todo.todo_id !== id));
     } catch (err) {
-      console.error(err.message)
+      console.error(err.message);
     }
-  }
+  };
 
-  const getTodos = async () =>{
+  const getTodos = async () => {
     try {
-
-      const response = await fetch("http://localhost:5000/todos")
+      const response = await fetch("http://localhost:5000/todos");
       const jsonData = await response.json();
 
       setTodos(jsonData);
     } catch (err) {
       console.error(err.message);
     }
-  }
-  useEffect(()=>{
+  };
+  useEffect(() => {
     getTodos();
-  },[])
+  }, []);
 
   return (
     <Fragment>
-        <table className="table mt-5 text-center">
+      <table className="table mt-5 text-center">
         <thead>
           <tr>
             <th>Description</th>
@@ -51,22 +50,27 @@ const ListTodos = () => {
           </tr>
           */}
           {todos.map(todo => (
-                <tr key={todo.todo_id}>
-                  <td>ID:{todo.todo_id} | {todo.description}</td>
-                  <td><EditTodo todo={todo}/></td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={()=>deleteTodo(todo.todo_id)}
-                      >
-                      Delete</button>
-                  </td>
-                </tr>
+            <tr key={todo.todo_id}>
+              <td>
+                ID:{todo.todo_id} | {todo.description}
+              </td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
     </Fragment>
-  )
+  );
 };
 
 export default ListTodos;
